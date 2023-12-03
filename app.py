fe441e844e9d5ab76c6bd312bcf910dd946e9717
Epifanes <131815652+Epifanes@users.noreply.github.com>
2023-12-03 16:25:43 +0000

    New interface

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 import streamlit as st
+from PIL import Image
 
-"""
-# Welcome to Digital Twin 2023-Data Visualisation - Bus drive
+def main():
+    st.title('Streamlit plotting of processed data')
+    """
+    Welcome to Digital Twin Høst 2023 - Data Processing and Visualization
 
-This app shows acceleration data recorded during a bus drive in Ålesund Sentrum 
+    This app shows acceleration data recorded during a bus drive in Ålesund Sentrum 
 
-Select from the sidebar menu to review the processed data you want!
+    Select from the sidebar menu to review the processed data you want!
 
-"""
+    """
+
+    # Images dictionary with names and file paths
+    images = {
+        'Acceleration data over time': '/workspaces/streamlit-echarts-demo/Images/connectedmesh2.PNG',
+        'Fourier analysis z-acceleration': 'path_to_your_image_2.png',
+        'Fourier analysis y-acceleration': 'path_to_your_image_2.png',
+        'Fourier analysis x-acceleration': 'path_to_your_image_2.png',
+        'Spectogram z-acceleration': 'path_to_your_image_3.png',
+        'Spectogram y-acceleration': 'path_to_your_image_3.png',
+        'Spectogram x-acceleration': 'path_to_your_image_3.png'
+    }
+
+    # Sidebar to select the image
+    selection = st.sidebar.selectbox('Select a result', list(images.keys()))
+
+    # Load and display the selected image
+    image_path = images[selection]
+    image = Image.open(image_path)
+    st.image(image, caption=selection, use_column_width=True)
+
+if __name__ == "__main__":
+    main()
