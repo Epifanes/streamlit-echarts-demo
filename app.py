2b5843a6f41062c022762cb91ad19e72edef2b6d
Epifanes <131815652+Epifanes@users.noreply.github.com>
2023-12-04 22:24:27 +0000

    New data

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ def main():
 
     # Images dictionary with names and file paths
     images = {
-        'Acceleration data over time': '/workspaces/streamlit-echarts-demo/Images/connectedmesh2.PNG',
-        'Fourier analysis z-acceleration': 'path_to_your_image_2.png',
-        'Fourier analysis y-acceleration': 'path_to_your_image_2.png',
-        'Fourier analysis x-acceleration': 'path_to_your_image_2.png',
-        'Spectogram z-acceleration': 'path_to_your_image_3.png',
-        'Spectogram y-acceleration': 'path_to_your_image_3.png',
-        'Spectogram x-acceleration': 'path_to_your_image_3.png'
+        'Acceleration data over time': '/workspaces/streamlit-echarts-demo/Images/Acc_over_time.png',
+        'Fourier analysis overall': '/workspaces/streamlit-echarts-demo/Images/Freq_spectrum.png',
+        'Fourier analysis z-acceleration': '/workspaces/streamlit-echarts-demo/Images/Fourier_z.png',
+        'Fourier analysis y-acceleration': '/workspaces/streamlit-echarts-demo/Images/Fourier_y.png',
+        'Fourier analysis x-acceleration': '/workspaces/streamlit-echarts-demo/Images/Fourier_x.png',
+        'Spectogram z-acceleration': '/workspaces/streamlit-echarts-demo/Images/Spectogram_z.png',
+        'Spectogram y-acceleration': '/workspaces/streamlit-echarts-demo/Images/Spectogram_y.png',
+        'Spectogram x-acceleration': '/workspaces/streamlit-echarts-demo/Images/Spectogram_x.png'
     }
 
     # Sidebar to select the image
